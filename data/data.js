0f45b69e3caf3be1ae25ab1c81d1303a860c92d0
Viacheslav Strogalev <strogalev@hotmail.com>
2024-06-24 22:19:33 +0300

    Google moves

--- data/data.js
+++ data/data.js
@@ -3,24 +3,24 @@ import { _gameStatuses } from "./constants.js";
 const _initialState = {
   points: {
     catch: 0,
-    miss: 0
+    miss: 0,
   },
   settings: {
     gridSize: {
       width: 4,
-      height: 4
+      height: 4,
     },
     pointsToWin: 20,
-    pointsToLose: 10
+    pointsToLose: 10,
   },
   googlePosition: {
     x: 0,
-    y: 0
+    y: 0,
   },
-  gameStatus: _gameStatuses.PLAY
-}
+  gameStatus: _gameStatuses.SETTINGS,
+};
 
-let _state = {..._initialState};
+let _state = { ..._initialState };
 
 let _Observer = () => {};
 
@@ -29,43 +29,59 @@ export function subscribe(cb) {
 }
 
 export function resetGame() {
-  _state = {..._initialState};
+  _state = { ..._initialState };
   _Observer();
 }
 
 export function getPoints() {
   return {
     catch: _state.points.catch,
-    miss: _state.points.miss
-  }
+    miss: _state.points.miss,
+  };
 }
 
 export function getGridSize() {
   return {
     width: _state.settings.gridSize.width,
-    height: _state.settings.gridSize.height
-  }
+    height: _state.settings.gridSize.height,
+  };
 }
 
 export function getGameStatus() {
-  return _state.gameStatus
+  return _state.gameStatus;
 }
 
 export function getGooglePosition() {
   return {
     x: _state.googlePosition.x,
-    y: _state.googlePosition.y
-  }
+    y: _state.googlePosition.y,
+  };
 }
 
 export function getPointsTo() {
   return {
     pointsToWin: _state.settings.pointsToWin,
-    pointsToLose: _state.settings.pointsToLose
-  }
+    pointsToLose: _state.settings.pointsToLose,
+  };
 }
 
 export function setStatus(newStatus) {
   _state.gameStatus = newStatus;
   _Observer();
 }
+
+function getNewPosition() {
+  let x, y;
+  do {
+    x = Math.floor(Math.random() * _state.settings.gridSize.width);
+    y = Math.floor(Math.random() * _state.settings.gridSize.width);
+  } while (_state.googlePosition.x === x && _state.googlePosition.y === y);
+  _state.googlePosition.x = x;
+  _state.googlePosition.y = y;
+}
+
+const googleMoves = setInterval(() => {
+  getNewPosition();
+  console.log(x, y);
+  _Observer();
+}, 1000);
